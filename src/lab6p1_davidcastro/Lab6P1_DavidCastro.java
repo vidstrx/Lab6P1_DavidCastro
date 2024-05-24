86255384095858961072f0a0fe29e0d5656ed3e7
vidstrx <dscastrogarcia@gmail.com>
2024-05-24 17:10:28 -0600

    Tercer commit

--- src/lab6p1_davidcastro/Lab6P1_DavidCastro.java
+++ src/lab6p1_davidcastro/Lab6P1_DavidCastro.java
@@ -35,20 +35,29 @@ public class Lab6P1_DavidCastro {
                     break;
                 case 2:
                     int vidas = 3;
-                    System.out.println("vidas: "+vidas);
                     int matrizSolucion[][] = new int[4][4];
                     char matrizX[][] = new char[4][4];
                     Matrices(matrizSolucion, matrizX);
-                    
-                    System.out.print("\nIngrese par de casillas: ");
-                    String casillas = read.next();
-                    char fila = casillas.charAt(0); char columna = casillas.charAt(2);
-                    char fila2 = casillas.charAt(4); char columna2 = casillas.charAt(6);
-                    int numfila = fila - '0';  int numcolumna = columna - '0';
-                    int numfila2 = fila2 - '0'; int numcolumna2 = fila - '0';
-                    
+                    do{
+                        System.out.println("vidas: "+vidas);
+                        
+
+                        System.out.print("\nIngrese par de casillas: ");
+                        String casillas = read.next();
+                        char fila = casillas.charAt(0); char columna = casillas.charAt(2);
+                        char fila2 = casillas.charAt(4); char columna2 = casillas.charAt(6);
+                        int numfila = fila - '0';  int numcolumna = columna - '0';
+                        int numfila2 = fila2 - '0'; int numcolumna2 = columna2 - '0';
+
+                        boolean mat = MatrizElementosIguales(matrizSolucion, matrizX, numfila, numcolumna, numfila2, numcolumna2);
+                        if (mat == false){
+                            vidas -=1;
+                        }
+                    }  while (vidas != 0);
                     break;
                 case 3:
+                    char c = (char) 3;
+                    
                     System.out.println("Saliste del programa");
                     break;
                 default:
@@ -60,10 +69,10 @@ public class Lab6P1_DavidCastro {
         } while (opcion != 3);
     }
     
-    public static void imprimirMatriz(int[][] arreglo){
-        for (int i = 0; i < arreglo.length; i++) {
-            for (int j = 0; j < arreglo[i].length; j++) {
-                System.out.print("["+arreglo[i][j] + "]"+" ");  
+    public static void imprimirMatriz(int[][] matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("["+matriz[i][j] + "]"+" ");  
             }
             System.out.println("");
         }
@@ -116,12 +125,16 @@ public class Lab6P1_DavidCastro {
         return matriz;
     }
     
+    // --------------------------------------Metodos del 2 ejercicio----------------------------------------------
+    
     public static char[][] Matrices(int[][] matriz4x4, char[][] matrizX){
         Random numRandom = new Random();
         for (int i = 0; i < matriz4x4.length; i++) {
             for (int j = 0; j < matriz4x4[i].length; j++) {
                 matriz4x4[i][j] = numRandom.nextInt(8);  
+                System.out.print("["+ matriz4x4[i][j]  + "]");
             }
+            System.out.println("");
         }
         for (int i = 0; i < matrizX.length; i++) {
             for (int j = 0; j < matrizX[i].length; j++) {
@@ -132,9 +145,39 @@ public class Lab6P1_DavidCastro {
         return matrizX;
     }
     
-    public static char[][] MatrizElementosIguales(int[][] matriz4x4, char[][] matrizX, int fila, int columna, int fila2, int columna2){
-        
-        return matrizX;
+    public static void imprimirMatrizChar(char[][] matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("["+matriz[i][j] + "]"+" ");  
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static boolean MatrizElementosIguales(int[][] matriz4x4, char[][] matrizX, int fila, int columna, int fila2, int columna2){
+        boolean matrizIgual;
+        int numero = matriz4x4[fila][columna];
+        int numero2 = matriz4x4[fila2][columna2];
+        char num = (char) numero;
+        System.out.println("Primera casilla: "+numero);
+        System.out.println("Segunda casilla: "+numero2);
+        for (int i = 0; i < matrizX.length; i++) {
+            for (int j = 0; j < matrizX[i].length; j++) {
+                if (numero == numero2){
+                    matrizX[fila][columna] = (char) numero;
+                    matrizX[fila2][columna2] = (char) numero2;
+                } 
+            }
+        }
+        if (numero == numero2){
+            System.out.println("Si son iguales");
+            matrizIgual = true;
+        } else {
+            System.out.println("No son iguales");
+            matrizIgual = false;
+        }
+        imprimirMatrizChar(matrizX);
+        return matrizIgual;
     }
     
 }
